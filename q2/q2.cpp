9b6ca01c4e34be015e8307df7abef8cf604fac3f
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-09 19:14:41 +0200

    maded print statement the same as that one of the quesiton

--- q2/q2.cpp
+++ q2/q2.cpp
@@ -5,5 +5,5 @@ using namespace std;
 
 int main() {
     int num1 = 10, num2 = 20;
-    cout << num1 << " + " << num2 << " = " << num1 + num2;
+    cout << "The sum of " << num1 << " and " << num2 " is : " << num1 + num2;
 }
