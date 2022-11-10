24abe3b362b88e67460e4b03f8326522c235a1c8
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-10 09:08:31 +0200

    finished q10

--- q10/quadratic.cpp
+++ q10/quadratic.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <vector>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
-float  calcHypotenuse(float a, float b, float c){
+vector<string> calcHypotenuse(float a, float b, float c){
+    vector<string> roots;
     float root1 = -b + sqrt(pow(b, 2) - (4 * a * c));
+    roots.push_back("x1: " + to_string(root1 / (2 * a)));
     float root2 = -b - sqrt(pow(b, 2) - (4 * a * c));
-    return root1;
+    roots.push_back("x2: " + to_string(root2 / (2 * a)));
+    return roots;
 }
 
 int main() {
 
-    float result = calcHypotenuse(2, 10, 2);
-    cout << result;    
+    vector<string> result = calcHypotenuse(5, 10, 3);
+    for(auto root: result) {
+        cout << root << endl;
+    }    
     return 0;
 }
