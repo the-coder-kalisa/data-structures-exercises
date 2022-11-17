98ecb7c561d3109c00213c3843e85d8d0a5f20c9
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-17 11:53:57 +0200

    finished oop and inheritance

--- lessons/tutorailWithMike.cpp
+++ lessons/tutorailWithMike.cpp
@@ -3,9 +3,23 @@
 
 using namespace std;
 
+class Chef{
+    public:
+        void makeChicken() {
+            cout << "This chef make chickent" << endl;
+        }
+};
+
+class ItalianChef : public Chef {
+    void makePasta() {
+        cout << "make pasta";
+    }
+};
+
 int main() {
-    int age  = 19;
-    int *page = &age;
-    cout << *page;
+    Chef chef;
+    chef.makeChicken();
+    ItalianChef italinaChef;
+    italinaChef.makeChicken();
     return 0;
 }
