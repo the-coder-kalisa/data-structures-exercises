5184e35fad8447f4d67041f2bf42fa0cbdf7f0d6
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-17 13:33:58 +0200

    finished q20

--- q20/palindrome.cpp
+++ q20/palindrome.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-#include <string.h>
 
 using namespace std;
 
+string reverse(string myString){
+    int strlen = myString.length();
+    int s = strlen - 1;
+    for (int i = 0; i < (strlen / 2); i++) {
+        char temp = myString[i];
+        myString[i] = myString[s];
+        myString[s] = temp;
+    }
+    return myString;
+}
+bool palindrome(string palind) {
+    string reversedString = reverse(palind);
+    return palind == reversedString;
+}
 int main() {
-    string palind = "hah";
-    string palindrome;
-    cout << str.length(palind);              
+    cout << palindrome("nun") << endl;
     return 0;
 }
