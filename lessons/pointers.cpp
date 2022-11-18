9531e4b33381d871d64f753334e454112dd0c326
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-18 09:35:37 +0200

    now understanding alittle about recusive

--- lessons/pointers.cpp
+++ lessons/pointers.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <array>
+#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-
+int power(int x, int n);
 
 int main() {
-   cout << "hello world";
+    cout << power(5, 3);
     return 0;
+}
+int power(int x, int n) {
+    cout << n << endl;
+    if (n == 0) {
+        return 1;
+    }
+    int smallOutput = power(x, n-1);
+    return x*smallOutput;
 }
--- lessons/recusive.cpp
+++ lessons/recusive.cpp
@@ -1,1 +1,8 @@
-#include <st
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
