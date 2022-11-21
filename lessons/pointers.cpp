8e06e6c91c1a5e86dcdeba39df50e99dd1a0af24
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-21 21:33:34 +0200

    add cpp

--- lessons/pointers.cpp
+++ lessons/pointers.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <array>
 #include <cmath>
+#include <string>
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int power(int x, int n);
+// int power(int x, int n);
+
+int sumDigitUsingLoop(int digit) {
+   string digitString = to_string(digit);
+   int sum = 0;
+   for (int i = 0; i < digitString.length(); i++) {
+        int number =  int (digitString[i]) - 48;
+        sum += number;
+   }
+    return sum;
+}
+
+int sumDigitUsingRecusive(int n) {
+    
+}
 
 int main() {
-    cout << power(5, 3);
+    cout << sumDigitUsingLoop(53);
     return 0;
-}
-int power(int x, int n) {
-    cout << n << endl;
-    if (n == 0) {
-        return 1;
-    }
-    int smallOutput = power(x, n-1);
-    return x*smallOutput;
 }
