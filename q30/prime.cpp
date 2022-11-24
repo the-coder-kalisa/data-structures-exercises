698807cb2086420a37b24faef2071eb90e2a433e
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-24 20:44:37 +0200

    increased the effieciency

--- q30/prime.cpp
+++ q30/prime.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
+    vector<int> primeNumbers = {};
+    int i = 2;
+    while (i <= 13)
+    {
+        int j = 2, numberOfProd = 0;
+        while (j < i)
+        {
+            if (i % j == 0)
+            {
+                if (numberOfProd > 0)
+                {
+                    break;
+                }
+                numberOfProd++;
+            }
+            j++;
+        }
+        if (numberOfProd == 0)
+        {
+            primeNumbers.push_back(i);
+        }
+        i++;
+    }
+    for (int i : primeNumbers)
+    {
+        // cout << i << endl;
+    }
     return 0;
 }
