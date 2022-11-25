403d15a1635943d5006fca2c56fc4605cbe9e9b8
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-25 22:40:55 +0200

    formatted

--- q4/SwappingNumbers.cpp
+++ q4/SwappingNumbers.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     // main funciton in q4
     int num1, num2, temp;
     cout << "Input 1st number: " << endl;
@@ -11,8 +12,8 @@ int main() {
     cin >> num2;
     temp = num2;
     num2 = num1;
-    num1 = temp;    
+    num1 = temp;
     cout << "After swapping the 1st number is : " << num1 << endl;
-    cout << "After swapping the 2nd number is : " << num2 << endl; 
+    cout << "After swapping the 2nd number is : " << num2 << endl;
     return 0;
 }
