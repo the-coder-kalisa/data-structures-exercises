0933b23500742f0414466731169a6a8fdaeaabc9
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-25 22:40:14 +0200

    formatted

--- lessons/pointers.cpp
+++ lessons/pointers.cpp
@@ -8,21 +8,24 @@ using namespace std;
 
 // int power(int x, int n);
 
-int sumDigitUsingLoop(int digit) {
-   string digitString = to_string(digit);
-   int sum = 0;
-   for (int i = 0; i < digitString.length(); i++) {
-        int number =  int (digitString[i]) - 48;
+int sumDigitUsingLoop(int digit)
+{
+    string digitString = to_string(digit);
+    int sum = 0;
+    for (int i = 0; i < digitString.length(); i++)
+    {
+        int number = int(digitString[i]) - 48;
         sum += number;
-   }
+    }
     return sum;
 }
 
-int sumDigitUsingRecusive(int n) {
-    
+int sumDigitUsingRecusive(int n)
+{
 }
 
-int main() {
+int main()
+{
     cout << sumDigitUsingLoop(53);
     return 0;
 }
