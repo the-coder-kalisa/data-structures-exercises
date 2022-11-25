64ff05fa0130f7a8a3edf74e4d7bce69f0c2cb69
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-25 22:41:16 +0200

    formatted

--- q5/volumeOfSphere.cpp
+++ q5/volumeOfSphere.cpp
@@ -4,12 +4,14 @@
 
 using namespace std;
 
-float calcuateVolume(int radius) {
+float calcuateVolume(int radius)
+{
     // function to calcultate volume
     return (4.0 / 3.0) * M_PI * pow(radius, 3);
 }
 
-int main() {
+int main()
+{
     int radius = 10;
     float volume = calcuateVolume(radius);
     cout << volume;
