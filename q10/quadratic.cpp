a3fe2a31e6d1b6d1e212c80e33b9471da1757be7
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-25 22:42:07 +0200

    formatted

--- q10/quadratic.cpp
+++ q10/quadratic.cpp
@@ -5,9 +5,10 @@
 
 using namespace std;
 
-vector<string> calcHypotenuse(float a, float b, float c){
+vector<string> calcHypotenuse(float a, float b, float c)
+{
     // function to calculate hypotenuse
-    
+
     vector<string> roots;
     float root1 = -b + sqrt(pow(b, 2) - (4 * a * c));
     roots.push_back("x1: " + to_string(root1 / (2 * a)));
@@ -16,11 +17,13 @@ vector<string> calcHypotenuse(float a, float b, float c){
     return roots;
 }
 
-int main() {
+int main()
+{
 
     vector<string> result = calcHypotenuse(5, 10, 3);
-    for(auto root: result) {
+    for (auto root : result)
+    {
         cout << root << endl;
-    }    
+    }
     return 0;
 }
