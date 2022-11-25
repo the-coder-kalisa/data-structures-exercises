f4b862de9d05acf737f305efdd9cfd959e281405
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-11-25 22:41:28 +0200

    formatted

--- q6/sumOfAllEvenNumbersLessthan100.cpp
+++ q6/sumOfAllEvenNumbersLessthan100.cpp
@@ -2,13 +2,16 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int i = 2, sum = 0;
-    while ( i < 100) {
-        if (i % 2 == 0) {   
-         // adding even number to eachother
-         
-         sum += i;
+    while (i < 100)
+    {
+        if (i % 2 == 0)
+        {
+            // adding even number to eachother
+
+            sum += i;
         }
         i++;
     }
